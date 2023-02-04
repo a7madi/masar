91e231570a3f58d37c71a73fe7e95fce84c8ca70
Abdulaziz Al Ahmadi <aziez@afahmadi.dev>
2023-02-04 11:52:42 +0300

    initiate project

---
+++ lib/model/trip.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: unused_field, prefer_final_fields
+
+import 'package:masar/model/destination.dart';
+import 'package:masar/model/carrier.dart';
+import 'package:masar/model/trip_owner.dart';
+
+enum TripStatus { onging, finished, draft }
+
+class Trip {
+  List<Destiniation> _tripList = [];
+  late TripOwner _tripOwner;
+  late TCompany _tCompany;
+  late String _tripNumber;
+  late TripStatus _tripStatus;
+
+  Trip({
+    required TripOwner tripOnwer,
+    required TCompany tCompany,
+    required List<Destiniation> tripList,
+    TripStatus tripStatus =TripStatus.draft,
+  }) {
+    _tCompany = tCompany;
+    _tripList = tripList;
+    _tripOwner = tripOnwer;
+    _tripStatus = tripStatus;
+    _tripNumber = DateTime.now().toIso8601String(); // سيتم تغيير آلية إنشاء رقم الرحلة لاحقًا
+  }
+}
