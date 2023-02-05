7bc34de34263829fbf4f13926f12d0c7fd8c77dd
Abdulaziz Al Ahmadi <a7madipro@gmail.com>
2023-02-05 16:07:14 +0300

    FIX: correcting file name spelling

---
+++ lib/model/transportation_company.dart
@@ -0,0 +1,7 @@
+class TCompany{
+  /// Transportation Company
+  final String name;
+  final String email;
+  final String phoneNumber;
+  TCompany({required this.name, this.email='', this.phoneNumber=''});
+}
