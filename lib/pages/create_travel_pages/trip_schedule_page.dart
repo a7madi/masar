53b15e919d1f1e4a363e402dc819ea17b6362b4e
Abdulaziz Al Ahmadi <aziez@afahmadi.dev>
2023-02-10 21:20:58 +0300

    Fix: Refactor code

--- lib/pages/create_travel_pages/trip_schedule_page.dart
+++ lib/pages/create_travel_pages/trip_schedule_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:masar/pages/create_travel_pages/widgets/form_text_field_widget.dart';
-import 'package:date_time_picker/date_time_picker.dart';
 
 class TripScheduleForm extends StatefulWidget {
   const TripScheduleForm({super.key});
@@ -10,7 +9,10 @@ class TripScheduleForm extends StatefulWidget {
 }
 
 class _TripScheduleFormState extends State<TripScheduleForm> {
+  DateTime _date = DateTime.now();
   var _pickedDate = '';
+  TimeOfDay _time = const TimeOfDay(hour: 0, minute: 0);
+  final _txtCntDateTime = TextEditingController();
   final _formKey = GlobalKey<FormState>();
   final _txtCntPickupLocation = TextEditingController();
   final _txtCntDropLocation = TextEditingController();
@@ -24,33 +26,82 @@ class _TripScheduleFormState extends State<TripScheduleForm> {
             child: Column(
               children: [
                 TxtFormField(
-                    textLable: 'نقطة التحميل', textController: _txtCntPickupLocation,maxInputDigits: 50,),
+                  textLable: 'نقطة التحميل',
+                  textController: _txtCntPickupLocation,
+                  maxInputDigits: 50,
+                ),
                 TxtFormField(
-                    textLable: 'نقطة التنزيل', textController: _txtCntDropLocation,maxInputDigits: 50,),
-                    _dateTimePicker(),
-
-                    TextButton(onPressed: (){}, child: const Text('أضف الوجهة')),
+                  textLable: 'نقطة التنزيل',
+                  textController: _txtCntDropLocation,
+                  maxInputDigits: 50,
+                ),
+                _dateTimePicker(),
+                TextButton(onPressed: () {}, child: const Text('أضف الوجهة')),
               ],
             ),
           )),
     );
   }
 
-  Padding _dateTimePicker() {
-    return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: DateTimePicker(dateMask: 'd MMM, yyyy',
-                    type: DateTimePickerType.dateTimeSeparate,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime.now(),
-                    lastDate: DateTime.now().add(const Duration(days: 365),),
-                      icon: const Icon(Icons.event),
-                    dateLabelText: 'حدد اليوم',
-                    dateHintText: 'حدد اليوم',
-                    timeHintText: 'حدد الوقت',
-                    onSaved: (value) => _pickedDate = value!,
-                    ),
-                  );
+  Widget _dateTimePicker() {
+    return Row(
+      children: [
+        TextButton(
+          onPressed: () {
+            Future.delayed(Duration.zero)
+                .then((_) => _setDate())
+                .then((_) => _setTime())
+                .then(
+                  (_) => _setPickedDate(),
+                );
+          },
+          child: const Text('حدد التاريخ والوقت'),
+        ),
+        SizedBox(
+          height: 80,
+          width: 150,
+          child: TextField(
+            readOnly: true,
+            controller: _txtCntDateTime,
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _setTime() async {
+    await showTimePicker(
+      context: context,
+      initialTime: const TimeOfDay(hour: 0, minute: 0),
+    ).then((value) {
+      if (value == null) {
+        return;
+      }
+      _time = value;
+    });
+  }
+
+  void _setDate() async {
+    var currentDate = DateTime.now();
+    await showDatePicker(
+      context: context,
+      initialDate: currentDate,
+      firstDate: currentDate,
+      lastDate: currentDate.add(
+        const Duration(days: 365),
+      ),
+    ).then((value) {
+      if (value == null) return;
+      _date = value;
+    });
+  }
+
+  void _setPickedDate() {
+    _pickedDate = '${(_time.hour > 9) ? _time.hour : '0' '${_time.hour}'}:${(_time.minute > 9) ? _time.minute : '0' '${_time.minute}'} - ${_date.toString().substring(0, 10).replaceAll('-', '/')}';
+
+    setState(() {
+      _txtCntDateTime.text = _pickedDate;
+    });
   }
 
   Card _destinationCard() {
@@ -76,5 +127,4 @@ class _TripScheduleFormState extends State<TripScheduleForm> {
       ),
     );
   }
-
 }
