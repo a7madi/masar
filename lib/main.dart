c91b8afa56b4a1190b12e72ac2a912081163d610
Abdulaziz Al Ahmadi <aziez@afahmadi.dev>
2023-02-13 21:04:13 +0300

    Feat: add TripDetailsProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:masar/pages/create_travel_pages/create_travel_page.dart';
 import 'package:masar/pages/home_page.dart';
+import 'package:masar/provider/trip_details_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,16 +12,19 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Masarak - مسارك',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (context) => TripDetailsProvider(),
+      child: MaterialApp(
+        title: 'Masarak - مسارك',
+        theme: ThemeData(
+    
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomePage(),
+        routes: {
+          CreateTravelPage.routeName: (_)=> const CreateTravelPage(),
+        },
       ),
-      home: const HomePage(),
-      routes: {
-        CreateTravelPage.routeName: (_)=> const CreateTravelPage(),
-      },
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:masar/pages/create_travel_pages/create_travel_page.dart';
+import '../provider/trip_details_provider.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -14,6 +16,9 @@ class HomePage extends StatelessWidget {
       body: Center(
           child: (TextButton(
               onPressed: () {
+                final providerData =
+                    Provider.of<TripDetailsProvider>(context, listen: false);
+                providerData.initializeNewTrip();
                 Navigator.of(context).pushNamed(CreateTravelPage.routeName);
               },
               child: const Text('أنشئ رحلة جديدة')))),
